278aca9bf80684c0092203752367e715334e4a75
Om Santosh Suneri <omsuneri@gmail.com>
2025-08-18 11:25:46 +0530

    Update test-demo.js

--- test-demo.js
+++ test-demo.js
@@ -1,2 +1,34 @@
-sm
-bbn
+// Test file to demonstrate Jacquez Action working
+// This file intentionally has some issues for testing
+
+export function testFunction() {
+  // TODO: This function needs proper documentation
+  console.log("Hello world");
+  return "test";
+}
+
+// Missing JSDoc comments and proper naming
+export const SOME_CONSTANT = "test";
+
+// This should trigger contributing guidelines violations
+function poorlyNamedFunc() {
+  var oldSyntax = "using var instead of const/let";
+  return oldSyntax;
+}
+
+// Another function without proper documentation
+function anotherFunction(param1, param2) {
+  return param1 + param2;
+}
+
+// Adding more test content to demonstrate the action
+class TestClass {
+  constructor() {
+    this.value = "test";
+  }
+  
+  // Method without JSDoc
+  getValue() {
+    return this.value;
+  }
+}
